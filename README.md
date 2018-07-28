59d66b6b581d488b45df98d3461d7f2e60e29eec
HokNan_Loue <34534604+190434957@users.noreply.github.com>
2018-07-28 21:40:23 +0800

    更新

--- README.md
+++ README.md
@@ -9,6 +9,15 @@
     cookie: 填入你的cookie
     userAgent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36
     firstUrl: /dashboard/xx/xxx //第一页
+    maxPage: 101
   ```
+
+  2. 第一次firstUrl获取方法，打开tumblr首页，打开开发者工具
+    找到request 为 https://www.tumblr.com/svc/dashboard/x/xxxx?nextAdPos=2&stream_cursor.....的URL
+    复制其中的/dashboard/x/xxxx即为第一次url
+
+  3. 同上 复制request中的cookie
+
+  4. maxPage为本次程序运行的最大页数，并将保存结束时的下次 firstURL，方便再次运行
   
-  运行main.js
+  5. 运行main.js
--- main.js
+++ main.js
@@ -11,12 +11,21 @@ const baseUrl = 'https://www.tumblr.com';
 const cookie = crawlerProperties.crawler.cookie;
 const userAgent = crawlerProperties.crawler.userAgent;
 const firstUrl = crawlerProperties.crawler.firstUrl;
+const maxPage = crawlerProperties.crawler.maxPage;
 
+/**
+ * 全局设置
+ */
 const agent = request
     .agent()
     .set('Cookie', cookie)
-    .set('User-Agent', userAgent)
+    .set('User-Agent', userAgent);
 
+/**
+ * 睡眠函数 防封
+ * @param time 毫秒数
+ * @returns {Promise<>} 没用
+ */
 function sleep(time) {
     return new Promise(function (resolve) {
         setTimeout(function () {
@@ -25,11 +34,19 @@ function sleep(time) {
     })
 }
 
-async function getPageImages(urlSet, pageNum, firstUrl, maxPage) {
+/**
+ * 图片url 递归
+ * @param urlSet 去重用url Set
+ * @param pageNum 页数
+ * @param firstUrl 开始url
+ * @param maxTurnPage 轮次最大页数
+ * @returns {Promise<*>} 一堆递归用的数据
+ */
+async function getPageImages(urlSet, pageNum, firstUrl, maxTurnPage) {
     console.log(`${pageNum} now ${urlSet.size} urls`)
     const res = await agent
         .get(`${baseUrl}/svc${firstUrl}`)
-        .proxy('http://127.0.0.1:1080')
+        .proxy('http://127.0.0.1:1080') //代理地址， 不需要代理请去掉
         .catch(err => console.log(err));
 
     /** @namespace res.body.response.DashboardPosts */
@@ -59,16 +76,22 @@ async function getPageImages(urlSet, pageNum, firstUrl, maxPage) {
     if (nextUrl == null) {
         debugger
     }
-    if (pageNum >= maxPage) {
+    if (pageNum >= maxTurnPage) {
         let srcUrlArr = Array.from(urlSet)
         console.log(`${srcUrlArr.length} URLs`);
         fs.appendFile(`./urls.json`, JSON.stringify(srcUrlArr));
         return {srcUrlArr, nextPage: pageNum + 1, nextUrl};
     }
     await sleep(200);
-    return await getPageImages(urlSet, ++pageNum, nextUrl, maxPage);
+    return await getPageImages(urlSet, ++pageNum, nextUrl, maxTurnPage);
 }
 
+/**
+ * 下载图片递归入口
+ * @param arr url数组
+ * @param idx 递归索引
+ * @returns {Promise<*>} 结束
+ */
 async function downloadImg(arr, idx) {
     if (idx >= arr.length) {
         return 'end img';
@@ -82,22 +105,34 @@ async function downloadImg(arr, idx) {
     const stream = fs.createWriteStream(`./pics/${fileName}`);
     await agent
         .get(url)
-        .proxy('http://127.0.0.1:1080')
+        .proxy('http://127.0.0.1:1080') //代理地址， 不需要代理请去掉
         .pipe(stream);
     await sleep(200);
     return await downloadImg(arr, ++idx);
 }
 
-async function mainEntry(pageNum, firstUrl, maxPage) {
+/**
+ * 轮次递归入口
+ * @param pageNum 开始页数
+ * @param firstUrl 递归开始url
+ * @param maxTurnPage 轮次最大页数
+ * @returns {Promise<*>} 本次结束位的下一次url
+ */
+async function mainEntry(pageNum, firstUrl, maxTurnPage) {
     console.log(`starting ${pageNum} page`)
-    if (pageNum >= 101 || firstUrl == null) {
+    if (pageNum >= maxPage || firstUrl == null) {
         return `end Url ${firstUrl}`;
     }
-    let res = await getPageImages(new Set(), pageNum, firstUrl, pageNum + maxPage - 1);
+    let res = await getPageImages(new Set(), pageNum, firstUrl, pageNum + maxTurnPage - 1);
     await downloadImg(res.srcUrlArr, 0);
-    return await mainEntry(res.nextPage, res.nextUrl, maxPage);
+    return await mainEntry(res.nextPage, res.nextUrl, maxTurnPage);
 }
 
+/**
+ * 开始执行程序
+ * 每轮爬取10页
+ * 返回值为本次结束位的下一次url ( crawler.yml -> crawler.firstUrl )
+ */
 mainEntry(1, firstUrl, 10)
     .then(res => {
         console.log(res)
